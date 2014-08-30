952674975642fb209c968a0538e0c5281f63eb25
Ivan Konorkin <konorkin.ivan@gmail.com>
2014-08-30 15:42:51 +0400

    Added parsing text into sentences

--- main.cpp
+++ main.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
+#include <set>
 using namespace std;
 
+class Sentence
+{
+    public:
+        string text;
+
+        Sentence(string _text) : text(_text) {}
+};
+
+class InputFile {
+    public:
+        InputFile(const char *path) {
+            file.open(path);
+        }
+
+        ~InputFile() {
+            file.close();
+        }
+
+        /**
+         * Get next sentence from input file.
+         *
+         * @return NULL if file is read, otherwise return ptr to next Sentence.
+         */
+        Sentence *nextSentence() {
+            string text = "";
+            char symbol; //TODO Support Unicode symbols
+            static char _end_characters[] = {'.', '!', '?', ';'};
+            static set<char> end_characters(_end_characters,
+                                            _end_characters + sizeof(_end_characters));
+            while (!file.eof()) {
+                symbol = file.get();
+                text += symbol;
+                if (end_characters.find(symbol) != end_characters.end()) {
+                    if (text.length() > 1) { // if sentence is not empty
+                        return new Sentence(text);
+                    } else {
+                        text = "";
+                    }
+                }
+            }
+            return NULL;
+        }
+    private:
+        ifstream file;
+};
+
 int
-main()
+main(int argc, char **argv)
 {
     return 0;
 }
